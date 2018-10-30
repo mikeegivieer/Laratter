980fd21b74463210ee0235163f21c376e9d51ba4
Miguel Angel Fuentes <mikeegivieer@gmail.com>
2018-10-30 10:21:04 -0600

    Seeding y Model Factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,3 +21,54 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+/*
+anteriormente ya se tiene una definición para usuarios
+comenzamos definido lo que es un mensaje, para agregar una definición
+utilizamos el objeto factory junto con su método define  que tiene 
+2 parámetros el primero es la clase que vamos a definir (mensaje)
+el segundo es una función anónima que va a recibir un objeto que 
+nos va a facilitar la creación de datos al azar, el objeto se llama Faker
+Dentro de la función anónima usamos Faker para generar los datos falsos y
+devolverlos en un array llave valor, (la llave es el nombre de la columna)
+*/
+
+$factory->define(App\Message::class, function (Faker $faker){
+
+    //realText es un método de faker que devuelve texto basado en alicia en el pais de las maravillas
+    //words otro métodos para palabras
+    //paragraph que devolverá un párrafo
+    //
+    return [
+        'content'=>$faker->realText(random_int(20,160)),
+        //imageUrl genera url de lorempixel
+        'image'=> $faker->imageUrl(600,338),
+    ];
+});
+
+/*
+para probar esto utilizaremos una herramienta de consola que proporciona laravel
+a través de artisan que se  llamada tinker
+Tenemos que verificar que tinker forme parte del proyectos en composer.json y que
+este incluida en config -> app -> service providers -> TinkerServiceProvoder
+*/
+
+/*
+En consola ejecutamos php artisan tinker, esto abrirá un loop entre leer 
+evaluar lo que escribimos, mostrar la salida y volver a empezar
+ Aquí usaremos la definición de addMessaje para construir un mensaje
+ Esto se hace a través de la función factory
+
+ >>factory(App\Message::class)->make(); construirá una instancia solamente
+>>factory(App\Message::class)->create; la va a construir y guardar en una base de datos
+
+Pero no vamos a crearlo usando tinker esto solo fue para probar la interacción con factory
+lo que vamos a usar son los seeds
+ 
+database-> DatabaseSeeder.php 
+
+
+
+
+
+*/
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -9,8 +9,29 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
+    /*
+    Laravel nos ofrece la capacidad de separar nuestros seeds en diferentes clases
+
+    */
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        //Hacemos lo mismo que hicimos en tinker
+        factory(App\Message::class)
+        ->times(100) // Para crear 100 mensajes
+        ->create();
     }
+   /*
+    Probamos esto desde artisan con 
+    php artisan db:seed
+    Listo, abra creado en base de datos todos los mensajes 
+    
+    Para borrar todos los datos en base de datos y subirlos otra vez
+
+    php artisan migrate:refresh --seed
+
+    Agregando --seed después de que termina de subir los datos ejecuta seed
+
+   */
+
 }
