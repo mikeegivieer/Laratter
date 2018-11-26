8b16c10fdbd5dde096a6adfd2fde3189025f54e1
Miguel Angel Fuentes <mikeegivieer@gmail.com>
2018-11-25 19:34:13 -0600

    Autorización para envío de mensajes

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -5,6 +5,12 @@
 <a href="{{$user->username}}/follows" class="btn btn-link">Sigue a <span class="badge badge-default" >{{$user->follows->count()}}</span></a>
 <a href="{{$user->username}}/followers"class="btn btn-link">Seguidores <span class="badge badge-default" >{{$user->followers->count()}}</span></a>
 @if (Auth::check())
+@if (Gate::allows('dms',$user))
+<form action="/{{$user->username}}/dms" method="POST">
+<input type="text" name="message" class="form-control">
+<button type="submit" class="btn btn-success" >Enviar DM</button>
+</form>
+@endif
     @if (Auth::user()->isFollowing($user))
     <form action="/{{$user->username}}/unfollow" method="post">
         {{ csrf_field() }}
