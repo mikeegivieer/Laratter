c25ad3652b5ed9164750e492a9ee1337ae7efc37
Miguel Angel Fuentes <mikeegivieer@gmail.com>
2018-12-15 19:14:20 -0600

    Incluye Laravel Debug Bar y optimiza tus consultas a base de datos

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -44,7 +44,7 @@ class MessagesController extends Controller
 
     public function search (Request $request){
         $query = $request->input('query');
-        $messages = Message::where('content','LIKE',"%$query%")->get();
+        $messages = Message::with('user')->where('content','LIKE',"%$query%")->get();
         return view('messages.index',[
           'messages'=> $messages,
         ]);
